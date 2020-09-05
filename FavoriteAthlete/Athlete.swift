0c25392f90e60381a6ba9f9f4ac134fd0c615103
codegangsk <kjhj0604@gmail.com>
2020-09-05 20:25:24 +0900

    create Athlete struct

---
+++ FavoriteAthlete/Athlete.swift
@@ -0,0 +1,19 @@
+//
+//  Athlete.swift
+//  FavoriteAthlete
+//
+//  Created by Sophie Kim on 2020/09/05.
+//
+
+import Foundation
+
+struct Athlete {
+    var name: String
+    var age: Int
+    var league: String
+    var team: String
+    
+    var description: String {
+        return "\(name) is \(age) years old and plays for the \(team) in the \(league)."
+    }
+}
